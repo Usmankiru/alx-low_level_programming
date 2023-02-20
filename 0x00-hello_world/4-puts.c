0374b9a34c8289ddbd513fc411cff86afe852d11
Usmankiru <usmanmukhtarisah@gmail.com>
2023-02-20 15:28:20 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that print a line with puts function
+ * return: 0 always successful
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle,");
+return (0);
+}
