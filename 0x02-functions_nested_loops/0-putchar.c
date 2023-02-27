def161577bfb8f0f709fb668f85fc068b038df05
Usmankiru <usmanmukhtarisah@gmail.com>
2023-02-27 14:38:20 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+ /**
+ * main - print_putchar
+ * this is code by kiru
+ * return 0
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('h');
+_putchar('h');
+_putchar('a');
+_putchar('r';
+_putchar('\n');
+return (0);
+}
+
